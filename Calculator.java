0b2d51c5ba3bd0dea65b9a202fcc1805cedbc554
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-07 12:26:31 +0530

    final

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,65 @@
-import java.util.*;
+import java.util.Scanner;
+
 class Calculator {
-public static void main(String [] args){
-Scanner sc = new Scanner(System.in);
-System.out.println("Welcome");
-System.out.println("Enter first number");
-double a = sc.nextDouble();
-System.out.println("Enter second number");
-double b = sc.nextDouble();
-System.out.println("Enter any function add, sub, mul, div");
-String coice = sc.next();
-}
-public static double add(double a, double b){
-return a+b;
-}
-public static double sub(double a, double b){
-return a-b;
-}
-public static double mul(double a, double b){
-return a*b;
-}
-public static double div(double a, double b){
-return a/b;
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Calculator!");
+        
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        
+        System.out.print("Enter any function (add, sub, mul, div): ");
+        String choice = sc.next();
+        
+        double result = 0.0;
+        boolean validChoice = true;
+        
+        switch (choice.toLowerCase()) {
+            case "add":
+                result = add(a, b);
+                break;
+            case "sub":
+                result = sub(a, b);
+                break;
+            case "mul":
+                result = mul(a, b);
+                break;
+            case "div":
+                if (b != 0) {
+                    result = div(a, b);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    validChoice = false;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation. Please choose from add, sub, mul, div.");
+                validChoice = false;
+        }
+        
+        if (validChoice) {
+            System.out.println("The result of " + choice + " operation is: " + result);
+        }
+        
+        sc.close();
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double sub(double a, double b) {
+        return a - b;
+    }
+
+    public static double mul(double a, double b) {
+        return a * b;
+    }
+
+    public static double div(double a, double b) {
+        return a / b;
+    }
 }
