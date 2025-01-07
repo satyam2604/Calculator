9abf8cf45e8fb2218acc86b537ebfd352a9dc61c
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-07 11:03:46 +0530

    subtract

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,17 @@ class Calculator {
 public static void main(String [] args){
 Scanner sc = new Scanner(System.in);
 System.out.println("Welcome");
+System.out.println("Enter first number");
 double a = sc.nextDouble();
+System.out.println("Enter second number");
 double b = sc.nextDouble();
+System.out.println("Enter any function add, sub, mul, div");
+String coice = sc.next();
 }
 public static double add(double a, double b){
 return a+b;
+}
+public static double sub(double a, double b){
+return a-b;
 }
 }
