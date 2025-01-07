866da63b5004a0040392dba8a206a87f085a3e9c
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-07 10:59:52 +0530

    input

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
+import java.util.*;
 class Calculator {
 public static void main(String [] args){
+Scanner sc = new Scanner(System.in);
 System.out.println("Welcome");
+double a = sc.nextDouble();
+double b = sc.nextDouble();
 }
 public static double add(double a, double b){
 return a+b;
